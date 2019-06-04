3d007e2cb440d489032c069e7783f3d38ebbd327
Bryan Wong <bryanw0ng@ucla.edu>
2019-06-04 15:20:32 -0700

    finish twin peaks except animating

--- src/scenes/twinpeaks.js
+++ src/scenes/twinpeaks.js
@@ -1,30 +1,64 @@
 // 80s shit scene
-var twinPeaksScene = new THREE.Scene();
-
-function Building(width, height, depth, x, y, z) {
-  const geom = new THREE.BoxGeometry(width, height, depth);
-  const mat = new THREE.MeshStandardMaterial();
-  const mesh = new THREE.Mesh(geom, mat);
-  mesh.position.set(x, y, z);
-  this.mesh = mesh;
-}
+var PixelShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    resolution: { value: null },
+    pixelSize: { value: 1 }
+  },
+  vertexShader: [
+    "varying highp vec2 vUv;",
+    "void main() {",
+    "vUv = uv;",
+    "gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+    "}"
+  ].join("\n"),
+  fragmentShader: [
+    "uniform sampler2D tDiffuse;",
+    "uniform float pixelSize;",
+    "uniform vec2 resolution;",
+    "varying highp vec2 vUv;",
+    "void main(){",
+    "vec2 dxy = pixelSize / resolution;",
+    "vec2 coord = dxy * floor( vUv / dxy );",
+    "gl_FragColor = texture2D(tDiffuse, coord);",
+    "}"
+  ].join("\n")
+};
 
 class TwinPeaksScene {
   constructor() {
     this.scene = new THREE.Scene();
+    this.postprocessing = false;
+    camera.position.z = 85;
   }
 
   setCar(obj) {
     // set position of passed in car object from common objects
+    obj.position.z = 60;
+    obj.position.y = -5;
+    obj.rotateY((7 * Math.PI) / 6);
+
+    obj.children[1].children[0].material = new THREE.MeshStandardMaterial({
+      emissive: 0x111111,
+      color: 0xf4cff3,
+      roughness: 0.5,
+      metalness: 1
+    });
+
+    obj.children[0].children[0].material = new THREE.MeshStandardMaterial({
+      //   emissive: 0xb20a02,
+      emissive: 0x810803,
+      color: 0xf4487a,
+      roughness: 0.5,
+      metalness: 1
+    });
+
+    console.log(obj);
+
+    return obj;
   }
 
   initScene() {
-    // car.children[0].material = new THREE.MeshStandardMaterial({
-    //   emissive: 0x888888,
-    //   color: 0xf4cff3,
-    //   roughness: 0.5,
-    //   metalness: 1
-    // });
     this.scene.add(this.setCar(car));
     this.scene.add(getPlatform());
     // this.scene.add(cliff);
@@ -37,21 +71,33 @@ class TwinPeaksScene {
     sunLight2.position.set(-10, 10, 0);
     this.scene.add(sunLight);
     this.scene.add(sunLight2);
+    const fogColor = new THREE.Color(0xf1efff);
+    this.scene.background = fogColor;
+    this.scene.fog = new THREE.FogExp2(fogColor, 0.01);
 
-    const numBuildings = 20;
-    this.buildings = [];
-    for (let i = 0; i < 20; i++) {
-      const b = new Building(
-        5,
-        Math.random() * 10 + 20,
-        5,
-        (2 * Math.random() - 1) * 50,
-        0,
-        30 + (2 * Math.random() - 1) * 5
-      );
-      this.buildings.push(b);
-      this.scene.add(b.mesh);
-    }
+    const globe = new THREE.Mesh(
+      new THREE.SphereGeometry(500),
+      new THREE.MeshStandardMaterial({
+        emissive: 0x996632
+      })
+    );
+    globe.position.set(0, -500, -50);
+    this.scene.add(globe);
+
+    // this.composer = new THREE.EffectComposer(renderer);
+    // this.composer.addPass(new THREE.RenderPass(this.scene, camera));
+    // const pixelPass = new THREE.ShaderPass(PixelShader);
+    // pixelPass.uniforms.resolution.value = new THREE.Vector2(
+    //   window.innerWidth,
+    //   window.innerHeight
+    // );
+    // pixelPass.uniforms.resolution.value.multiplyScalar(window.devicePixelRatio);
+    // pixelPass.renderToScreen = true;
+    // this.composer.addPass(pixelPass);
+    // this.postprocessing = true;
+
+    const city = new THREEx.ProceduralCity();
+    this.scene.add(city);
     camera.position.z = 85;
   }
 
