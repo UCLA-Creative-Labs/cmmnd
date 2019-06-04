5c8a5320232bb71a81e1c8e766f8b0abf3bb2c5f
Bryan Wong <bryanw0ng@ucla.edu>
2019-06-04 12:56:07 -0700

    changed twinpeaks to class

--- src/scenes/twinpeaks.js
+++ src/scenes/twinpeaks.js
@@ -9,39 +9,61 @@ function Building(width, height, depth, x, y, z) {
   this.mesh = mesh;
 }
 
-function initTwinPeaksScene() {
-  car.children[0].material = new THREE.MeshStandardMaterial({
-    emissive: 0x888888,
-    color: 0xf4cff3,
-    roughness: 0.5,
-    metalness: 1
-  });
-  twinPeaksScene.add(cliff);
-
-  const sunLight = new THREE.DirectionalLight(0xe1edf0, 0.2);
-  sunLight.castShadow = true;
-  sunLight.position.set(10, 10, 0);
-  const sunLight2 = new THREE.DirectionalLight(0xe1edf0, 0.2);
-  sunLight2.castShadow = true;
-  sunLight2.position.set(-10, 10, 0);
-  twinPeaksScene.add(sunLight);
-  twinPeaksScene.add(sunLight2);
-
-  const numBuildings = 20;
-  buildings = [];
-  for (let i = 0; i < 20; i++) {
-    const b = new Building(
-      5,
-      Math.random() * 10 + 20,
-      5,
-      (2 * Math.random() - 1) * 50,
-      0,
-      30 + (2 * Math.random() - 1) * 5
-    );
-    buildings.push(b);
-    twinPeaksScene.add(b.mesh);
+class TwinPeaksScene {
+  constructor() {
+    this.scene = new THREE.Scene();
   }
-  camera.position.z = 85;
+
+  setCar(obj) {
+    // set position of passed in car object from common objects
+  }
+
+  initScene() {
+    // car.children[0].material = new THREE.MeshStandardMaterial({
+    //   emissive: 0x888888,
+    //   color: 0xf4cff3,
+    //   roughness: 0.5,
+    //   metalness: 1
+    // });
+    this.scene.add(this.setCar(car));
+    this.scene.add(getPlatform());
+    // this.scene.add(cliff);
+
+    const sunLight = new THREE.DirectionalLight(0xe1edf0, 0.2);
+    sunLight.castShadow = true;
+    sunLight.position.set(10, 10, 0);
+    const sunLight2 = new THREE.DirectionalLight(0xe1edf0, 0.2);
+    sunLight2.castShadow = true;
+    sunLight2.position.set(-10, 10, 0);
+    this.scene.add(sunLight);
+    this.scene.add(sunLight2);
+
+    const numBuildings = 20;
+    this.buildings = [];
+    for (let i = 0; i < 20; i++) {
+      const b = new Building(
+        5,
+        Math.random() * 10 + 20,
+        5,
+        (2 * Math.random() - 1) * 50,
+        0,
+        30 + (2 * Math.random() - 1) * 5
+      );
+      this.buildings.push(b);
+      this.scene.add(b.mesh);
+    }
+    camera.position.z = 85;
+  }
+
+  update(pitch_array) {
+    // update objects within the scene
+  }
+
+  animate() {
+    const pitch_array = audio.getFreqData();
+    this.update(pitch_array);
+    // calls update
+  }
 }
 
 function animateTwinPeaks() {}
